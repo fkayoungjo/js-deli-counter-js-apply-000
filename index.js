36b9f758b7ae40740fb55216d7c0388ac03c1bad
Jordan Simon <jordan.simon55@gmail.com>
2018-08-23 13:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ function currentLine(line) {
     return 'The line is currently empty.'
   } else {
     var string = 'The line is currently:';
-    for (var i = 0; i < line.length; i++) }
-    return `${string} ${line[i]}`
+    for (var i = 0; i < line.length; i++)
+    {return `${string} ${line[i]}`
+  }
 
+}
 }
